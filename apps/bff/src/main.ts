fbb45918612e35ae9ee50657b6ae46e507d94e6a
trantuananh-17 <anhkyohauik17@gmail.com>
2026-02-05 00:05:56 +0700

    chore/#: done config cors and setup prettier

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -9,37 +9,41 @@ import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
-  app.setGlobalPrefix(globalPrefix);
-  // Điều kiện để validate và chuyển đổi kiểu dữ liệu
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  try {
+    const app = await NestFactory.create(AppModule);
+    const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
+    app.setGlobalPrefix(globalPrefix);
+    // Điều kiện để validate và chuyển đổi kiểu dữ liệu
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  const config = new DocumentBuilder()
-    .setTitle('Stationery-bff API')
-    .setDescription('The Stationery-bff API description')
-    .setVersion('1.0.0')
-    .addBearerAuth({
-      description: 'Default JWT Authorization',
-      type: 'http',
-      in: 'header',
-      scheme: 'bearer',
-      bearerFormat: 'JWT',
-      name: 'Authorization',
-    })
-    .build();
+    app.enableCors({
+      origin: '*',
+    });
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+    const config = new DocumentBuilder()
+      .setTitle('Stationery-bff API')
+      .setDescription('The Stationery-bff API description')
+      .setVersion('1.0.0')
+      .addBearerAuth({
+        description: 'Default JWT Authorization',
+        type: 'http',
+        in: 'header',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+      })
+      .build();
 
-  const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}/docs`,
-  );
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+
+    const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
+    await app.listen(port);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}/docs`);
+  } catch (error) {
+    Logger.error(`Application failed to start: ${error}`, '', 'Bootstrap', false);
+  }
 }
 
 bootstrap();
