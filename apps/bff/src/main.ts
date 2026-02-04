f58ce99da228af6eef4a7e825606ad34af7a56b6
trantuananh-17 <anhkyohauik17@gmail.com>
2026-02-04 23:58:25 +0700

    chore/#swagger: setup swagger

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,10 +14,31 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   // Điều kiện để validate và chuyển đổi kiểu dữ liệu
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Stationery-bff API')
+    .setDescription('The Stationery-bff API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Default JWT Authorization',
+      type: 'http',
+      in: 'header',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+    })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+
   const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}/docs`,
   );
 }
 
